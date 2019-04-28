b00598e84e32a5fa7f75484b13d5e0b4da061d5a
anirudh_jwala <anirudhsai2@gmail.com>
2019-04-28 15:49:34 +0530

    Initial commit

---
+++ app/src/main/java/com/anirudh/smartreply/MainActivity.java
@@ -0,0 +1,21 @@
+package com.anirudh.smartreply;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anirudh.smartreply.chat.ChatFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, ChatFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
